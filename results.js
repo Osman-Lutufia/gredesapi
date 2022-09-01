2a6dd8b6aa66fe6bfad723b32e26a99dc167ce5e
Osman-Lutufia <osmanlutufia11@gmail.com>
2022-09-01 15:11:55 -0700

    first commit

---
+++ results.js
@@ -0,0 +1,9 @@
+const results = [
+  { name: "Osman Lutufia", class: "form three", grade: "10" },
+  { name: "Osman Hafiz", class: "form three", grade: "25" },
+  { name: "Fuseini Zakia", class: "form three", grade: "12" },
+  { name: "Ali Mohmmed", class: "form three", grade: "16" },
+  { name: "Samsudeen Naeem", class: "form three", grade: "20" },
+];
+
+module.exports = results;
